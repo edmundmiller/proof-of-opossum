ecce2ddfcbf8044ab0e8d24e3dcdec77ded59645
sharad-s <sharadshekar@gmail.com>
2019-02-17 04:16:12 -0600

    subgraph works

--- graph/src/mapping.ts
+++ graph/src/mapping.ts
@@ -3,8 +3,8 @@ import { StatusName } from './types/schema'
 
 
 export function handleCreation(event: Creation): void {
-  let statusName = new StatusName(event.params.id.toHex())
-  statusName.owner = event.params.addr
+  let statusName = new StatusName(event.params.Id.toHex())
+  statusName.owner = event.params._from
   statusName.displayName = event.params.name
   statusName.amount = event.params.amount
   statusName.save()
