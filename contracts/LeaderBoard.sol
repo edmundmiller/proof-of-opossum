c72a382c03ace79d16e1d64527e50219e0898624
Edmund Miller <edmund.a.miller@protonmail.com>
2019-02-16 23:54:51 -0600

    Add initial LeaderBoard

---
+++ contracts/LeaderBoard.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.3;
+
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Burnable.sol';
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Mintable.sol';
+
+contract LeaderBoard is ERC20Burnable, ERC20Mintable {
+
+    struct StatusName {
+        address addr;
+        uint amount;
+        bytes32 name;
+    }
+
+    uint next_nameId = 0;
+    mapping(uint => statusName) public TheBoard;
+
+    function newStatusName(bytes32 name) public {
+        StatusName storage newName = TheBoard[next_nameId];
+
+        newName.addr = msg.sender;
+        newName.amount = 0;
+        newName.name = name;
+
+        next_nameId ++;
+    }
+
+    function tokenMint() public returns (bool) {
+        _mint(msg.sender, 10);
+        return true;
+    }
+
+    function vote(uint nameId) public {
+        require(burnFrom(msg.sender, 1), "Not enough tokens");
+
+        TheBoard[next_nameId].amount ++;
+    }
+}
