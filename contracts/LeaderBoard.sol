a18523eab6190fa332b41e8af3dd48eda4d0ad90
Edmund Miller <edmund.a.miller@protonmail.com>
2019-02-17 01:33:16 -0600

    Add events

--- contracts/LeaderBoard.sol
+++ contracts/LeaderBoard.sol
@@ -4,9 +4,9 @@ import 'openzeppelin-eth/contracts/token/ERC20/ERC20Burnable.sol';
 import 'openzeppelin-eth/contracts/token/ERC20/ERC20Mintable.sol';
 
 contract LeaderBoard is ERC20Burnable, ERC20Mintable {
-    event Creation(address indexed _from, uint keyId);
-    event Request(address indexed _from, uint keyId);
-    event Authorize(address indexed _from, uint keyId, address node);
+    event Creation(address indexed _from, uint amount, bytes32 name, uint Id);
+    event Mint(address indexed _from, uint amount);
+    event Vote(address indexed _from, uint nameId, bytes32 name, uint total);
 
     struct StatusName {
         address addr;
@@ -24,16 +24,19 @@ contract LeaderBoard is ERC20Burnable, ERC20Mintable {
         newName.amount = 0;
         newName.name = name;
 
+        emit Creation(newName.addr, newName.amount, newName.name, next_nameId);
         next_nameId ++;
     }
 
     function tokenMint() public returns (bool) {
         _mint(msg.sender, 10);
+        emit Mint(msg.sender, 10);
         return true;
     }
 
     function vote(uint nameId) public {
         burnFrom(msg.sender, 1);
         TheBoard[nameId].amount ++;
+        emit Vote(msg.sender, nameId, TheBoard[nameId].name, TheBoard[nameId].amount);
     }
 }
