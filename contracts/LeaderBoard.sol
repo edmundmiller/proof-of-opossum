a03d0f6047bc08dd6d16836d3bb4c4437c4f1e0c
Edmund Miller <edmund.a.miller@protonmail.com>
2019-02-17 06:35:23 -0600

    Fix LeaderBoard

--- contracts/LeaderBoard.sol
+++ contracts/LeaderBoard.sol
@@ -4,26 +4,29 @@ import 'openzeppelin-eth/contracts/token/ERC20/ERC20Burnable.sol';
 import 'openzeppelin-eth/contracts/token/ERC20/ERC20Mintable.sol';
 
 contract LeaderBoard is ERC20Burnable, ERC20Mintable {
-    event Creation(address indexed _from, uint amount, bytes32 name, uint Id);
+    event Creation(address indexed _from, uint amount, bytes32 name, uint nameId);
     event Mint(address indexed _from, uint amount);
     event Vote(address indexed _from, uint nameId, bytes32 name, uint total);
 
     struct StatusName {
         address addr;
-        uint amount;
+        uint256 amount;
         bytes32 name;
     }
 
-    uint next_nameId = 0;
+    uint256 next_nameId = 0;
     mapping(uint => StatusName) public TheBoard;
+    mapping(address => bool) public IsInTheBoard;
 
     function newStatusName(bytes32 name) public {
+        require(IsInTheBoard(msg.sender) != true);
         StatusName storage newName = TheBoard[next_nameId];
 
         newName.addr = msg.sender;
         newName.amount = 0;
         newName.name = name;
 
+        IsInTheBoard(msg.sender) = true;
         emit Creation(newName.addr, newName.amount, newName.name, next_nameId);
         next_nameId ++;
         tokenMint(msg.sender);
@@ -38,5 +41,9 @@ contract LeaderBoard is ERC20Burnable, ERC20Mintable {
         burn(1);
         TheBoard[nameId].amount ++;
         emit Vote(msg.sender, nameId, TheBoard[nameId].name, TheBoard[nameId].amount);
+    }
+
+    function loadStatus(uint256 nameId) public {
+        return TheBoard[nameId];
     }
 }
